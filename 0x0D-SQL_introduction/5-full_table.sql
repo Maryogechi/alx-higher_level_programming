d8da1f8e069ee1ea77c31ded47828d696c0ac083
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-05-25 15:14:31 +0100

    full table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- create table in current database of the mysql server
+-- create a table 'first_table' in current DB
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+)
