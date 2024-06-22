3c2f30718c6f573b30b0122770d4e1b8122cef99
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-06-22 17:27:11 +0100

    task

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.log('Two arguments are required');
+} else {
+  console.log(`${args[0]} is ${args[1]}`);
+}
