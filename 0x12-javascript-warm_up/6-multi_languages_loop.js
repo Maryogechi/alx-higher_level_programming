b8529906b1f1234775b7738c13f75daa743ea8f6
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-06-23 17:58:33 +0100

    task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Define an array of strings
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Use a loop to iterate over the array and print each message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
