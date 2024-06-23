400d10429a1fd01f24ef0831cfe3177d1f06304f
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-06-23 17:52:56 +0100

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,17 @@
 
 // a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 
-function isInteger (str) {
-  return parseInt(str) === Number(str);
+const args = process.argv.slice(2); // Get command-line arguments, excluding the first two default ones
+
+// Function to check if a string can be converted to an integer
+const isInteger = (str) => {
+  const num = Number(str);
+  return Number.isInteger(num);
+};
+
+// Check if the first argument is passed and if it can be converted to an integer
+if (args[0] !== undefined && isInteger(args[0])) {
+  console.log(`My number: ${parseInt(args[0], 10)}`);
+} else {
+  console.log('Not a number');
 }
-console.log(isInteger('first argument converted in integer'));
-console.log(isInteger('Not a number'));
