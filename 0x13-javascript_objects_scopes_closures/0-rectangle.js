d8d5f15fd75e3e525a90b60d15f9c1653802c167
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-06-24 20:45:54 +0100

    task

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-  // Class methods and properties will go here
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  // Method to calculate area
+  calculateArea () {
+    return this.width * this.height;
+  }
 }
+const myRectangle = new Rectangle(5, 3);
+console.log(myRectangle.calculateArea());
