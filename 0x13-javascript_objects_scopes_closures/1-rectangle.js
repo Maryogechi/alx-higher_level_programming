2ba84acc465419d1ffa964763bc91e1519d39fd5
Maryogechi <iheanaetumaryogechi@gmail.com>
2024-06-24 21:39:32 +0100

    task

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -7,17 +7,18 @@ class Rectangle {
 }
 
 // Test cases with specified output
-const rect1 = new Rectangle(2, 3);
-console.log(rect1); // Output: Rectangle { width: 2, height: 3 }
-console.log(rect1.width); // Output: 2
-console.log(rect1.height); // Output: 3
 
-const rect2 = new Rectangle(2, -3);
-console.log(rect2); // Output: Rectangle { width: 2, height: -3 }
-console.log(rect2.width); // Output: 2
-console.log(rect2.height); // Output: -3
+const r1 = new Rectangle(2, 3);
+console.log(r1); // Output: Rectangle { width: 2, height: 3 }
+console.log(r1.width); // Output: 2
+console.log(r1.height); // Output: 3
 
-const rect3 = new Rectangle(2);
-console.log(rect3); // Output: Rectangle { width: 2, height: undefined }
-console.log(rect3.width); // Output: 2
-console.log(rect3.height); // Output: undefined
+const r2 = new Rectangle(2, -3);
+console.log(r2); // Output: Rectangle { width: 2, height: -3 }
+console.log(r2.width); // Output: 2
+console.log(r2.height); // Output: -3
+
+const r3 = new Rectangle(2);
+console.log(r3); // Output: Rectangle { width: 2, height: undefined }
+console.log(r3.width); // Output: 2
+console.log(r3.height); // Output: undefined
